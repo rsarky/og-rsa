cf86dff16de4aeb2ee75458922faac9c6ffac74e
Rohit Sarkar <rohitsarkar5398@gmail.com>
2020-04-10 19:12:51 +0530

    fix bug, ensure d is prime

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,53 @@ use num_traits::{Zero, One};
 use std::cmp::Ordering;
 use std::io;
 
+struct Key {
+    exponent: BigUint,
+    base: BigUint
+}
+
+#[derive(Debug)]
+struct KeySet {
+    e: BigUint,
+    d: BigUint,
+    n: BigUint
+}
+
+impl KeySet {
+    fn gen_key() -> KeySet {
+        let mut rng1 = rand::thread_rng();
+        let mut rng2 = rand::thread_rng();
+        let p = gen_prime(100);
+        let q = gen_prime(100);
+        let n = &p*&q;
+        let maxpq = match p.cmp(&q){ // TODO Find better way to do this
+            Ordering::Less => &q,
+            Ordering::Greater => &p,
+            Ordering::Equal => &p
+        };
+        let phi = (&p-1u32)*(&q-1u32);
+
+        // d need to be coprime to phi. A prime greater than max(p,q) should do
+        let d = gen_prime_above(100, &maxpq);
+        println!("{} {}", d, phi);
+        let e = mult_inverse(&phi, &d);
+
+        KeySet {
+            e,
+            d,
+            n
+        }
+    }
+
+    fn encrypt(&self, msg: u32) -> BigUint {
+        msg.to_biguint().unwrap().modpow(&self.e, &self.n)
+    }
+
+    fn decrypt(&self, cipher: &BigUint) -> BigUint {
+        cipher.modpow(&self.d, &self.n)
+    }
+
+}
 // reference implementation of modular exponentiation for 32 bit numbers.
 fn mod_pow(base: u32, exponent: u32, modulus: u32) -> u32 {
     if modulus == 1 {
@@ -25,41 +72,27 @@ fn mod_pow(base: u32, exponent: u32, modulus: u32) -> u32 {
     result
 }
 
-fn encrypt(msg: u32, e: &BigUint, n: &BigUint) -> BigUint {
-    // These will replaced by 2 large randomly generated primes
-    // TODO Should this be bigint?
-    msg.to_biguint().unwrap().modpow(&e, &n)
-}
 
-fn decrypt(cipher: &BigUint, d: &BigUint, n: &BigUint) -> BigUint {
-    cipher.modpow(&d, &n)
-}
 
-fn gen_key() -> (BigUint, BigUint, BigUint) {
-    let mut rng1 = rand::thread_rng();
-    let mut rng2 = rand::thread_rng();
-    let p = gen_prime();
-    let q = gen_prime();
-    let n = &p*&q;
-    let maxpq = match p.cmp(&q){ // TODO Find better way to do this
-        Ordering::Less => &q,
-        Ordering::Greater => &p,
-        Ordering::Equal => &p
-    };
-    let phi = (&p-1u32)*(&q-1u32);
-    let d = rng1.gen_biguint_range(&maxpq, &(maxpq + rng2.gen_biguint(100)));
-    let e = mult_inverse(&phi, &d);
-    (e,d,n) // e,n => public key. d,n => private key.
-}
-
-fn gen_prime() -> BigUint {
-    let num_bits = 100;
+fn gen_prime(num_bits: usize) -> BigUint {
     let mut rng = rand::thread_rng();
     let mut a = rng.gen_biguint(num_bits);
+
+    // TODO: Better way required.
     while !is_prime(&a) {
         a = rng.gen_biguint(num_bits);
     }
     a
+}
+
+fn gen_prime_above(num_bits: usize, lbound: &BigUint) -> BigUint {
+    let mut rng = rand::thread_rng();
+    let mut a = rng.gen_biguint(num_bits);
+
+    while !is_prime(&(&a+lbound)) {
+        a = rng.gen_biguint(num_bits);
+    }
+    a+lbound
 }
 
 fn is_prime(num: &BigUint) -> bool {
@@ -81,7 +114,7 @@ fn is_prime(num: &BigUint) -> bool {
 }
 
 fn mult_inverse(a: &BigUint, b: &BigUint) -> BigUint {
-    // mult inverse of a under b
+    // mult inverse of b under a
     let mut s0: BigInt = Zero::zero();
     let mut s1: BigInt = One::one();
     let mut r0 = a.clone();
@@ -97,14 +130,13 @@ fn mult_inverse(a: &BigUint, b: &BigUint) -> BigUint {
     }
 
     while s0 < Zero::zero() {
-        s0 = s0 + BigInt::from_biguint(Sign::Plus, b.clone());
-        println!("falal");
+        s0 = s0 + BigInt::from_biguint(Sign::Plus, a.clone());
     }
     s0.to_biguint().expect("Something is fishy!")
 }
 
 fn main() {
-    let (e,d,n) = gen_key();
+    let key_set = KeySet::gen_key();
     let mut input = String::new();
     println!("Enter a positive integer");
     io::stdin().read_line(&mut input)
@@ -112,8 +144,8 @@ fn main() {
     let input: u32 = input.trim().parse()
         .expect("Please enter a valid integer.");
     println!("You entered: {}", input);
-    let enc = encrypt(input, &e, &n);
-    let dec = decrypt(&enc, &d, &n);
-    println!("{} {} {}", e, d, n);
+    let enc = key_set.encrypt(input);
+    let dec = key_set.decrypt(&enc);
     println!("{} {}",enc,dec);
+
 }
